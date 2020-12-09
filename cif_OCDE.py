17d766dcdbaa451e440b88f3dd5372c3a1ee12a3
Jérémie Stym-Popper <jeremstym@gmail.com>
2020-12-09 21:31:50 +0100

    Création d'un usa_bon_df, avec les variables adéquates

--- cif_OCDE.py
+++ cif_OCDE.py
@@ -7,15 +7,25 @@ Module pour importer les données de l'OCDE et visualiser le tableau
 @author: Jérémie Stym-Popper
 """
 
-import cif as cf
+from cif import cif
+import pandas as pd
 
-database = cf.createOneCountryDataFrameFromOECD('USA', 'MEI', 
+database = cif.createOneCountryDataFrameFromOECD('USA', 'MEI', 
                                      frequency='Q', startDate='1990-Q1')
 
 usa_df = database[0]
 labelvar = database[1]
 
-path_csv = ""
-database[1].to_csv(path_csv)
+#path_csv = ""
+#database[1].to_csv(path_csv)
        
-       
+##--- Création d'un dataframe pour USA ----
+
+liste_var = ['NAEXKP01', 'LREMTTTT', 'LRACTTTT', 'LRUNTTTT', 
+             'NAEXKP02', 'NAEXKP04', 'NAEXKP06']
+
+headers = ['PIB', 'Emplois', 'Actifs', 'Chomage', 'Conso', 
+           'Formation', 'Exports']
+
+d = {col:var_df for col, var_df in zip(headers, [usa_df['{0}'.format(var)]["STSA"] for var in liste_var] )}
+usa_bon_df = pd.DataFrame(d)
