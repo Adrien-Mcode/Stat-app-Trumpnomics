f0f25dde8df6fe521adad11f7ca99b15218dde96
Jérémie Stym-Popper <jeremstym@gmail.com>
2021-01-28 19:02:32 +0100

    Améliration de la table ocde_opti

--- OCDE_optim.py
+++ OCDE_optim.py
@@ -7,3 +7,19 @@ Travail sur la base de l'OCDE
 @author: Jérémie Stym-Popper
 """
 
+# Création d'un dataframe pour l'analyse des variables par pays
+
+import pandas as pd
+
+ocde_df = pd.read_csv(r"https://raw.githubusercontent.com/Adrien-Mcode/Stat-app-Trumpnomics/main/ocde2.csv?token=ARARNIBTC46VS7K7TZW2TM3ACL2X4")
+
+ocde_df.rename(columns={'Unnamed: 0':'Variables'}, inplace=True)
+
+ind_tuple = list(zip(ocde_df['Pays'], ocde_df['Variables']))
+new_index = pd.MultiIndex.from_tuples(ind_tuple, names=["Pays", "Variables"])
+
+tocde = ocde_df.T.copy()
+tocde.columns=new_index
+tocde.drop(['Variables', 'Pays'],inplace=True)
+
+tocde.to_csv(r"C:\Users\Asus\Desktop\Jérémie\Fac_ENSAE\Stat app'\Stat-app-Trumpnomics\ocde_opti.csv")
