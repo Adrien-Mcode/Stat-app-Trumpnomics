ef76bf4543404b0bf590c6c1452c16c7fa79da22
Adrien-Mcode <62481170+Adrien-Mcode@users.noreply.github.com>
2021-02-25 23:44:44 +0100

    mise en forme du df pour le module de controle synthétique
    changement de la forme du dataframe pour correspondre au standard du package (il faut encore déplacer les colonnes cependant)

---
+++ adaptation_df.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 17:57:04 2021
+
+@author: SURFACE
+"""
+import SyntheticControlMethods as scm
+import pandas as pd
+
+df = pd.read_csv('ocde_df.csv',header = [0,1])
+
+pays_ocde = {"Germany" :'DEU',"Australia" :'AUS',"Austria":'AUT',"Belgium":'BEL',"Canada":'CAN',"Denmark":'DNK',"Spain":'ESP',"Finland":'FIN',"France":'FRA',"Greece":'GRC',"Ireland":'IRL',"Italy":'ITA',
+             "Japan":'JPN',"Luxembourg":'LUX',"Norway":'NOR',"New-Zealand":'NZL',"Netherlands":'NLD',"Portugal":'PRT',"United Kingdom":'GBR',"Sweden":'SWE',"Switzerland":'CHE',"Turkey":'TUR',"United-States":'USA'}
+
+'''
+INPUT VARIABLES:
+        
+dataset: the dataset for the synthetic control procedure.
+Should have the the following column structure:
+ID, Time, outcome_var, x0, x1,..., xn
+Each row in dataset represents one observation.
+The dataset should be sorted on ID then Time. 
+That is, all observations for one unit in order of Time, 
+followed by all observations by the next unit also sorted on time
+        
+ID: a string containing a unique identifier for the unit associated with the observation.
+E.g. in the simulated datasets provided, the ID of the treated unit is "A".
+        
+Time: an integer indicating the time period to which the observation corresponds.
+        
+treated_unit: ID of the treated unit
+'''
+df_ct = pd.DataFrame()
+for i in pays_ocde.keys() :
+    interm = df[['Pays',i]].drop([(i,'income p0p50'),(i,'income p90p100')],
+                                 axis = 1)
+    interm['ID'] = i
+    interm.columns = interm.columns.droplevel()
+    df_ct = pd.concat([df_ct,interm],axis = 0)
+
+df_ct.to_csv('df_scm.csv')
