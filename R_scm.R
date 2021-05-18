e6731d2fa9d0d1b023f2ec6efb157d73a9a195dd
AdrienVcode <adx.vallette@sfr.fr>
2021-05-18 20:56:01 +0200

    sans accents

--- R_scm.R
+++ R_scm.R
@@ -6,9 +6,9 @@ library(stringr)
 
 
 
-##########   Import des données    ###########
+##########   Import des donnees    ###########
 
-#Après avoir importer la base, on observe sa forme puis on met la variable "TIME" au format numérique.
+#Apres avoir importer la base, on observe sa forme puis on met la variable "TIME" au format numerique.
 data <- read.csv("dfR_complete_netexp.csv")
 summary(data)
 
@@ -22,7 +22,7 @@ data$LOCATION <- as.character(data$LOCATION)
 summary(data)
 
 
-#Création de la liste des prédicteurs
+#Creation de la liste des predicteurs
 list_predictor <- rep(list(list()), 261)
 
 #PIB
@@ -41,15 +41,15 @@ for (k in 1:87) {
 ########## MODELE BASELINE #########
 
 #On utilise d'abord la fonction dataprep() du package "Synth", 
-#qui va préparer les vecteurs et matrices utilisées pour trouver la pondération. 
-#x1 est le vecteur pour l'unité traité (les Etats-Unis) des 3 séries temporelles et des 5 covariables, 
-#X0, la matrice équivalente pour les unités de contrôle (le donor pool), 
-#Z1 le vecteur de la variable d'intérêt (ici le taux de chômage) pour l'unité traité 
-#et Z0 la matrice de la variable d'intérêt pour chacun des pays du donor pool.
+#qui va preparer les vecteurs et matrices utilisees pour trouver la ponderation. 
+#x1 est le vecteur pour l'unite traite (les Etats-Unis) des 3 series temporelles et des 5 covariables, 
+#X0, la matrice equivalente pour les unites de controle (le donor pool), 
+#Z1 le vecteur de la variable d'interet (ici le taux de chomage) pour l'unite traite 
+#et Z0 la matrice de la variable d'interet pour chacun des pays du donor pool.
 
 
 
-##########   Création des vecteurs du problème d'optimisation    ###########
+##########   Creation des vecteurs du probleme d'optimisation    ###########
 dataprep.out <- dataprep(
   foo = data,
   predictors = c("Chomage", "Conso_share", "Invest_share", 'NExport_share', 'Labor_prod'),
@@ -70,14 +70,14 @@ dataprep.out <- dataprep(
 ##########   Vecteurs et matrices obtenus    ###########
 dataprep.out$X1  #Vecteur des US
 dataprep.out$X0  #Matrice du donor pool
-dataprep.out$Z1  #Vecteur du taux de chômage US sur la période d'entraînement
-dataprep.out$Z0  #Matrice des taux de chômage des unités de contrôle sur la période d'entraînement
+dataprep.out$Z1  #Vecteur du taux de chomage US sur la periode d'entrainement
+dataprep.out$Z0  #Matrice des taux de chomage des unites de controle sur la periode d'entrainement
 
 
-##########   Résolution du problème d'optimisation et résultats    ###########
-#Résolution du problème de minisation à l'aide de la fonction synth() du package "Synth".
-#Elle nous donne en sortie les valeurs prédites par le contrôle synthétique pour chacune des variables de la minimisation, 
-#la pondération W du contrôle synthétique et la pondération V des variables.
+##########   Resolution du probleme d'optimisation et resultats    ###########
+#Resolution du probleme de minisation a l'aide de la fonction synth() du package "Synth".
+#Elle nous donne en sortie les valeurs predites par le controle synthetique pour chacune des variables de la minimisation, 
+#la ponderation W du controle synthetique et la ponderation V des variables.
 
 
 start.time <- Sys.time()
@@ -90,22 +90,22 @@ time.taken
 
 synth.tables <- synth.tab(dataprep.res = dataprep.out, synth.res = synth.out)
 gaps <- dataprep.out$Y1plot - (dataprep.out$Y0plot %*% synth.out$solution.w)
-#Ecart entre le taux de chômage prédit par le contrôle synthétique et celui observé des US
+#Ecart entre le taux de chomage predit par le controle synthetique et celui observe des US
 
-head(synth.tables$tab.pred, 5) #Tableau des prédictions comparées aux valeurs observées
-head(synth.tables$tab.v, 5)    #Tableau des pondérations des variables
-synth.tables$tab.w            #Tableau des pondérations des pays
+head(synth.tables$tab.pred, 5) #Tableau des predictions comparees aux valeurs observees
+head(synth.tables$tab.v, 5)    #Tableau des ponderations des variables
+synth.tables$tab.w            #Tableau des ponderations des pays
 
 
-##########   Graphiques du modèle baseline    ###########
+##########   Graphiques du modele baseline    ###########
 path.plot(synth.res = synth.out, dataprep.res = dataprep.out, Ylab = "Chomage", Xlab = "year", Legend = c("USA", "synthetic"), Legend.position = "topright")
 abline(v = 2016.75, col="blue", lwd=3, lty=2)
 gaps.plot(synth.res = synth.out, dataprep.res = dataprep.out, Ylab = "gap in unemployment rate", Main = NA)
 abline(v = 2016.75, col="blue", lwd=3, lty=2)
 
-#Pour le taux de chômage, le PIB, l'emploi et les actifs, on trace les courbes obtenus pour les Etats-Unis et le contrôle synthétique,
-#l'intervalle de confiance étant obtenu par + ou - 2 fois l'écart-type de la différence entre les vraies valeurs 
-#et celles prédites sur la période pré-traitement.
+#Pour le taux de chomage, le PIB, l'emploi et les actifs, on trace les courbes obtenus pour les Etats-Unis et le controle synthetique,
+#l'intervalle de confiance etant obtenu par + ou - 2 fois l'ecart-type de la difference entre les vraies valeurs 
+#et celles predites sur la periode pre-traitement.
 
 #Plot du PIB 
 
@@ -121,18 +121,18 @@ lines(data$TIME[1:101], graph_bl$USA_pib, col="red", type = "l", lty=1, lwd=4)
 lines(data$TIME[1:101], pib_p, col="black",lty=2)
 lines(data$TIME[1:101], pib_m, col="black",lty=2)
 abline(v = 2016.75, col="green", lwd=3, lty=2)
-legend(1995, 1, legend=c("USA", "contrôle synthétique", "élection de Trump"), col=c("blue", "red", "green"), lty=1)
+legend(1995, 1, legend=c("USA", "controle synthetique", "election de Trump"), col=c("blue", "red", "green"), lty=1)
 
 #Plot du chomage
 chom = graph_bl$sc_cho - 2*sd(graph_bl$USA_cho - graph_bl$sc_cho)
 chop = graph_bl$sc_cho + 2*sd(graph_bl$USA_cho - graph_bl$sc_cho)
 
-plot(data$TIME[1:101], graph_bl$USA_cho, ylab = 'Taux de chômage', xlab = 'Time', ylim = c(0,10), col="blue", type = "l", lty = 1, lwd=4)
+plot(data$TIME[1:101], graph_bl$USA_cho, ylab = 'Taux de chomage', xlab = 'Time', ylim = c(0,10), col="blue", type = "l", lty = 1, lwd=4)
 lines(data$TIME[1:101], graph_bl$sc_cho, col="red", type = "l", lty=1, lwd=4)
 lines(data$TIME[1:101], chom, col="black",lty=2)
 lines(data$TIME[1:101], chop, col="black",lty=2)
 abline(v = 2016.75, col="green", lwd=3, lty=2)
-legend(1995, 10, legend=c("USA", "contrôle synthétique", "élection de Trump"), col=c("blue", "red", "green"), lty=1)
+legend(1995, 10, legend=c("USA", "controle synthetique", "election de Trump"), col=c("blue", "red", "green"), lty=1)
 
 
 #Plot de l'emploi
@@ -144,7 +144,7 @@ lines(data$TIME[1:101], graph_bl$sc_emp, col="red", type = "l", lty=1, lwd=4)
 lines(data$TIME[1:101], empm, col="black",lty=2)
 lines(data$TIME[1:101], empp, col="black",lty=2)
 abline(v = 2016.75, col="green", lwd=3, lty=2)
-legend(1995, 0.8, legend=c("USA", "contrôle synthétique", "élection de Trump"), col=c("blue", "red", "green"), lty=1)
+legend(1995, 0.8, legend=c("USA", "controle synthetique", "election de Trump"), col=c("blue", "red", "green"), lty=1)
 
 #Plot des actifs
 actm = graph_bl$sc_act - 2*sd(graph_bl$USA_act - graph_bl$sc_act)
@@ -155,7 +155,7 @@ lines(data$TIME[1:101], graph_bl$sc_act, col="red", type = "l", lty=1, lwd=4)
 lines(data$TIME[1:101], actm, col="black",lty=2)
 lines(data$TIME[1:101], actp, col="black",lty=2)
 abline(v = 2016.75, col="green", lwd=3, lty=2)
-legend(1995, 0.8, legend=c("USA", "contrôle synthétique", "élection de Trump"), col=c("blue", "red", "green"), lty=1)
+legend(1995, 0.8, legend=c("USA", "controle synthetique", "election de Trump"), col=c("blue", "red", "green"), lty=1)
 
 ##########   Tests de robustesse    ###########
 
@@ -195,7 +195,7 @@ lines(dataprep.out$tag$time.plot, d$y0plot14, col = 'grey', type = 'l',lwd=2, lt
 lines(dataprep.out$tag$time.plot, d$y0plot16, col = 'grey', type = 'l',lwd=2, lty=3)
 lines(dataprep.out$tag$time.plot, d$y0plot22, col = 'grey', type = 'l',lwd=2, lty=3)
 abline(v = 2016.75, col="black", lwd=3, lty=2)
-legend(1995, 10, legend=c("USA", "contrôle synthétique", "élection de Trump", "leave-one-out"),
+legend(1995, 10, legend=c("USA", "controle synthetique", "election de Trump", "leave-one-out"),
        col=c("red", "blue", "black", "grey"), lty=c(1,2,2,3,3,3))
 
 # Space placebos
@@ -219,7 +219,7 @@ plot_placebos(
   discard.extreme = FALSE,
   mspe.limit = 20,
   xlab = 'Time',
-  ylab = 'Différence de taux de chômage entre prédicteurs et vraies valeurs pour chacun des pays',
+  ylab = 'Difference de taux de chomage entre predicteurs et vraies valeurs pour chacun des pays',
   title = "Placebo chomage",
   alpha.placebos = 1,
 )
@@ -271,9 +271,9 @@ lines(data$TIME[1:101], graph_bl$sc_cho, col="red", type = "l", lty=1, lwd=4)
 lines(data$TIME[1:101], chom, col="grey",lty=2)
 lines(data$TIME[1:101], chop, col="grey",lty=2)
 
-##########   Modèle alternatif    ###########
+##########   Modele alternatif    ###########
 
-# on reprend le code du modèle baseline en inversant chômage et PIB
+# on reprend le code du modele baseline en inversant chomage et PIB
 
 list_pred_pib <- rep(list(list()), 261)
 
@@ -319,7 +319,7 @@ head(s_pib.tables$tab.pred, 5)
 head(s_pib.tables$tab.v, 5)
 s_pib.tables$tab.w
 
-# Graphiques des résultats
+# Graphiques des resultats
 
 graph_pib <- read.csv("graph_pib.csv")
 gaps_pib2 <- data$PIB[1718:1804] - graph_pib$sc_pib[1:87]
@@ -332,19 +332,19 @@ lines(data$TIME[1:101], graph_pib$sc_pib, col="red", type = "l", lty=1, lwd=2)
 lines(data$TIME[1:101], pib_p2, col="black",lty=2)
 lines(data$TIME[1:101], pib_m2, col="black",lty=2)
 abline(v = 2016.75, col="green", lwd=3, lty=2)
-legend(1995, 1, legend=c("USA", "contrôle synthétique", "élection de Trump"), col=c("blue", "red", "green"), lty=1)
+legend(1995, 1, legend=c("USA", "controle synthetique", "election de Trump"), col=c("blue", "red", "green"), lty=1)
 
 chom2 = graph_pib$sc_cho - 2*sd(data$Chomage[1718:1804] - graph_pib$sc_cho)
 chop2 = graph_pib$sc_cho + 2*sd(data$Chomage[1718:1804] - graph_pib$sc_cho)
 
-plot(data$TIME[2:101], data$Chomage[1718:1817], ylab = 'Taux de chômage', xlab = 'Time', ylim = c(0,10), col="blue", type = "l", lty = 1, lwd=4)
+plot(data$TIME[2:101], data$Chomage[1718:1817], ylab = 'Taux de chomage', xlab = 'Time', ylim = c(0,10), col="blue", type = "l", lty = 1, lwd=4)
 lines(data$TIME[1:101], graph_pib$sc_cho, col="red", type = "l", lty=1, lwd=4)
 lines(data$TIME[1:101], chom2, col="black",lty=2)
 lines(data$TIME[1:101], chop2, col="black",lty=2)
 abline(v = 2016.75, col="green", lwd=3, lty=2)
-legend(1995, 10, legend=c("USA", "contrôle synthétique", "élection de Trump"), col=c("blue", "red", "green"), lty=1)
+legend(1995, 10, legend=c("USA", "controle synthetique", "election de Trump"), col=c("blue", "red", "green"), lty=1)
 
-# Tests de robustesse pour le modèle alternatif
+# Tests de robustesse pour le modele alternatif
 
 tdf2 <- generate.placebos(dp_pib.out,s_pib.out, Sigf.ipop = 2, strategy = "multiprocess")
 
@@ -364,7 +364,7 @@ plot_placebos(
   discard.extreme = FALSE,
   mspe.limit = 20,
   xlab = 'Time',
-  ylab = 'Différence de PIB entre prédicteurs et vraies valeurs pour chacun des pays',
+  ylab = 'Difference de PIB entre predicteurs et vraies valeurs pour chacun des pays',
   title = "Placebo PIB",
   alpha.placebos = 1,
 )
